f06540f16d12a36a523b4cf2f708244e320544a0
Wilson <wilson.diant@gmail.com>
2024-12-26 17:26:11 -0500

    Fix environment path for .env file and update Flask blueprint URL prefix

--- services/flask_route.py
+++ services/flask_route.py
@@ -38,7 +38,7 @@ server.config.from_object(APIConfig)
 api = Api(server)
 
 
-stops = Blueprint("stops", "api", description="Operations on stops")
+stops = Blueprint("stops", "api", url_prefix="/api", description="Operations on stops")
 
 
 @stops.route("/")
@@ -64,4 +64,4 @@ class StopsCollection(MethodView):
 api.register_blueprint(stops)
 
 if __name__ == "__main__":
-    server.run()
+    server.run(port=6543, debug=True)
