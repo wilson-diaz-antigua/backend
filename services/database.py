a3aa6426e423667ba4dd4059885c4ea5fb9c680d
Wilson <wilson.diant@gmail.com>
2025-03-01 14:38:53 -0500

    Restrict CORS to specific origin for enhanced security

--- services/database.py
+++ services/database.py
@@ -20,7 +20,7 @@ vercel = vals.get("VERCEL_URL")
 db = SQLAlchemy()
 ma = Marshmallow()
 server = Flask(__name__)
-CORS(server)
+CORS(server, resources={r"/*": {"origins": "https://www.willdiaz.me"}})
 server.config["CACHE_TYPE"] = "SimpleCache"
 server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 migrate = Migrate(server, db)
